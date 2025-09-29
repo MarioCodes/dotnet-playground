82d7e12963553ebbda1860e53fbf30fd77f7fb96
Mario Codes <mariocodes1993@gmail.com>
2025-09-29 16:05:28 +0200

    moved ef core poc from templates

---
+++ efcore-and-middleware/efcore/Middleware/MiddlewareService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using efcore.Middleware.interfaces;
+
+namespace efcore.Middleware
+{
+    public class MiddlewareService : IMiddlewareService
+    {
+        public async Task ProcessRequest(string request)
+        {
+            // do something with the request
+            Console.WriteLine(request);
+        }
+
+    }
+}
