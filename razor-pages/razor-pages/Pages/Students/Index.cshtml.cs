fcd66e95e9196a8a6781073d3240accb3f2f11f2
Mario Codes <mariocodes1993@gmail.com>
2025-10-28 13:27:28 +0100

    add sorting

--- razor-pages/razor-pages/Pages/Students/Index.cshtml.cs
+++ razor-pages/razor-pages/Pages/Students/Index.cshtml.cs
@@ -12,18 +12,45 @@ namespace RazorPages.Pages.Students
 {
     public class IndexModel : PageModel
     {
-        private readonly RazorPages.Data.SchoolContext _context;
+        private readonly SchoolContext _context;
 
-        public IndexModel(RazorPages.Data.SchoolContext context)
+        public IndexModel(SchoolContext context)
         {
             _context = context;
         }
 
-        public IList<Student> Student { get;set; } = default!;
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public IList<Student> Students { get; set; }
+
+
+        public async Task OnGetAsync(string sortOrder)
         {
-            Student = await _context.Students.ToListAsync();
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            IQueryable<Student> studentsIQ = from s in _context.Students select s;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            Students = await studentsIQ.AsNoTracking().ToListAsync();
         }
     }
 }
