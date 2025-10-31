69eb4046529f6886341f72ff0c845c33e275f097
Mario Codes <mariocodes1993@gmail.com>
2025-10-31 12:19:02 +0100

    use basic local configuration

--- razor-pages-azure-webapp/RazorPagesWebapp/RazorPagesWebapp/Pages/Index.cshtml.cs
+++ razor-pages-azure-webapp/RazorPagesWebapp/RazorPagesWebapp/Pages/Index.cshtml.cs
@@ -5,16 +5,21 @@ namespace RazorPagesWebapp.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _config;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public string Message { get; set; }
+
+        public IndexModel(IConfiguration config, 
+            ILogger<IndexModel> logger)
         {
+            _config = config;
             _logger = logger;
         }
 
         public void OnGet()
         {
-
+            Message = _config["MyCustomConfig:IndexText"] ?? "";
         }
     }
 }
